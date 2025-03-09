f8defcf1647eeb99ba5aab0be4989763e21b72ff
Muhammad Syifa Surya Saputra <syifamuhammad3139@gmail.com>
2025-03-09 19:07:35 +0700

    `Update layout and page components with new title and styles`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Dashboard from "@/components/Dashboard";
+import Hero from "@/components/Hero";
+import Kontak from "@/components/Kontak";
+import ManajemenKolam from "@/components/ManajemenKolam";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+      <ManajemenKolam />
+      <Dashboard />
+      <Kontak />
+    </div>
+  );
 }
